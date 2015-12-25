23a6bcb3d9dc7e695c3f2b76a0318fed6bd45547
kusti8 <kusti8@gmail.com>
2015-12-25 09:35:11 -0500

    Add fahrenheit conversion

--- env_scroll.py
+++ env_scroll.py
@@ -6,11 +6,13 @@ while True:
     p = sense.get_pressure()
     h = sense.get_humidity()
 
+    t = 9.0/5.0*t+32
+
     t = round(t, 1)
     p = round(p, 1)
     h = round(h, 1)
 
-    msg = "Temperature = %s, Pressure=%s, Humidity=%s" % (t,p,h)
+    msg = "Temperature = %s F, Pressure=%s mb, Humidity=%s %" % (t,p,h)
     print(t,p,h)
 
     sense.show_message(msg)
