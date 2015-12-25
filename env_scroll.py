f2536c9b8d9d460dbc398b67c00eba04e86304b3
kusti8 <kusti8@gmail.com>
2015-12-25 09:38:58 -0500

    Add celcius print debug

--- env_scroll.py
+++ env_scroll.py
@@ -5,14 +5,14 @@ while True:
     t = sense.get_temperature()
     p = sense.get_pressure()
     h = sense.get_humidity()
-
+    print t
     t = 9.0/5.0*t+32
 
     t = round(t, 1)
     p = round(p, 1)
     h = round(h, 1)
 
-    msg = "Temperature = %s F, Pressure=%s mb, Humidity=%s %%" % (t,p,h)
+    msg = "Temperature = %s F, Pressure=%s mb, Humidity=%s%%" % (t,p,h)
     print(t,p,h)
 
     sense.show_message(msg)
