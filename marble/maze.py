47174bd1781b57a7402cdeaf7f8846f3964eb303
kusti8 <kusti8@gmail.com>
2015-12-25 11:12:22 -0500

    Add maze display

---
+++ marble/maze.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+sense.clear()
+
+r = (255,0,0)
+b = (0,0,0)
+
+maze = [[r,r,r,r,r,r,r,r],
+        [r,b,b,b,b,b,b,r],
+        [r,r,r,b,r,b,b,r],
+        [r,b,r,b,r,r,r,r],
+        [r,b,b,b,b,b,b,r],
+        [r,b,r,r,r,r,b,r],
+        [r,b,b,r,b,b,b,r],
+        [r,r,r,r,r,r,r,r]]
+        
+snese.set_pixels(sum(maze,[]))
