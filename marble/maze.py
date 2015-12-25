c8b6c0ff70c127c15c6dd4958b9bb852eb61e050
kusti8 <kusti8@gmail.com>
2015-12-25 11:14:18 -0500

    Modify maze a little

--- marble/maze.py
+++ marble/maze.py
@@ -6,7 +6,7 @@ r = (255,0,0)
 b = (0,0,0)
 
 maze = [[r,r,r,r,r,r,r,r],
-        [r,b,b,b,b,b,b,r],
+        [r,b,b,b,b,b,r,r],
         [r,r,r,b,r,b,b,r],
         [r,b,r,b,r,r,r,r],
         [r,b,b,b,b,b,b,r],
