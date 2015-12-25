f7e9edcb3342b3f2f5ae5987bfc26d6a7f5bafa1
kusti8 <kusti8@gmail.com>
2015-12-25 11:16:05 -0500

    Add marble

--- marble/maze.py
+++ marble/maze.py
@@ -4,6 +4,12 @@ sense.clear()
 
 r = (255,0,0)
 b = (0,0,0)
+w = (255,255,255)
+
+x = 1
+y = 1
+
+game_over = False
 
 maze = [[r,r,r,r,r,r,r,r],
         [r,b,b,b,b,b,r,r],
@@ -15,3 +21,7 @@ maze = [[r,r,r,r,r,r,r,r],
         [r,r,r,r,r,r,r,r]]
 
 sense.set_pixels(sum(maze,[]))
+
+while not game_over:
+    maze[y][x] = w
+    sense.set_pixels(sum(maze,[]))
